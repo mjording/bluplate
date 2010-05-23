d9b1bb8f498115aee85754fe80b9aea2688bd6e8
matthew jording <mjording@opengotham.com>
2010-05-22 21:31:11 -0400

    commit some other specs

--- spec/bluplate_spec.rb
+++ spec/bluplate_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Bluplate" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should have a user" do
+    user = User.new
+    user.class.should == User
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'bluplate'
 require 'spec'
 require 'spec/autorun'
+require 'task'
+require 'user'
 
 Spec::Runner.configure do |config|
   
