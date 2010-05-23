f45c34dcc26503b6cb601276d7a14f536b7f2197
matthew jording <mjording@opengotham.com>
2010-05-23 01:09:42 -0400

    activesupport for the win

--- lib/persist.rb
+++ lib/persist.rb
@@ -2,12 +2,13 @@ require 'rubygems'
 require 'mongo'
 require 'settings'
 require 'json'
+require 'active_support/inflector'
 
 module Persist
   include Mongo
   DB = Mongo::Connection.new(Settings.mongo.host).db(Settings.mongo.db)
   def save
-    DB.collection(self.class.to_s.downcase).insert(self.to_hash)
+    DB.collection(self.class.to_s.downcase.pluralize).insert(self.to_hash)
   end 
 end
 
